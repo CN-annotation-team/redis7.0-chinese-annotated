876ca77ee377d59ad6077b95bee54882dda0f766
Lu JJ <275955589@qq.com>
2022-06-02 06:56:35 +0800

    Update src/sentinel.c

--- src/sentinel.c
+++ src/sentinel.c
@@ -81,7 +81,7 @@ typedef struct sentinelAddr {
                                            this master. */
 /* 实例被选中为主服务器（当前仍是从服务器）*/
 #define SRI_PROMOTED (1<<7)            /* Slave selected for promotion. */
-/* 向服务器发送 SLAVEOF命令，让其转向复制新主服务器 */
+/* 向服务器发送 SLAVEOF 命令，让其转向复制新主服务器 */
 #define SRI_RECONF_SENT (1<<8)     /* SLAVEOF <newmaster> sent. */
 /* 从服务器正在和主服务器进行同步 */
 #define SRI_RECONF_INPROG (1<<9)   /* Slave synchronization in progress. */
