0806e75a878790348cc6bcaa92420f4d1187af2b
xiaoconglin <53688888+xiaoconglin@users.noreply.github.com>
2022-06-08 20:44:19 +0800

    Update src/sds.h
    Co-authored-by: Binbin <binloveplay1314@qq.com>

--- src/sds.h
+++ src/sds.h
@@ -42,11 +42,10 @@ extern const char *SDS_NOINIT;
 
 typedef char *sds; /* Simple Dynamic String 简单动态字符串 */
 
-/* __packed__ 取消结构在编译过程中的优化对齐，按照实际占用字节数进行对齐
- * SDS 数据结构定义 <len><alloc><flags><buf>
- * 特性1 指针是直接指向buf 使得可以直接使用 C 语言string.h库中的函数
- * 特性2 并且前进一位就能直接获取flags。从而快速拿到类型
- * redis 为了上述两个特性 牺牲了对齐填充
+/* __packed__ 取消结构在编译过程中的优化对齐，按照实际占用字节数进行内存分配
+ * SDS 数据结构定义：<len><alloc><flags><buf>
+ * 特性1：sds 指针是直接指向 buf 字节数组，使得 SDS 可以直接使用 C 语言 string.h 库中的函数
+ * 特性2：并且根据 buf 前进一位就能直接获取 flags，从而快速拿到类型，例如 `unsigned char flags = s[-1]`
  * */
 
 /* 注意: sdshdr5 从未被使用, 我们仅访问 flags 字节.
